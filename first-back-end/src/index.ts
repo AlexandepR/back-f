3716abc9d1ad3a7256148caf75ad26b10094bef3
Alex <alexandepp@gmail.com>
2022-06-09 21:21:28 +0300

    index.ts

--- first-back-end/src/index.ts
+++ first-back-end/src/index.ts
@@ -1,10 +1,65 @@
-const express = require('express')
-const app = express()
-const port = 3001
+import express, {Request, Response} from 'express'
+import cors from 'cors'
+import bodyParser from "body-parser";
 
-app.get('/', (req: any, res: any) => {
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+const port = process.env.PORT || 3001
+
+const videos = [
+    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+]
+
+app.get('/', (req: Request, res: Response) => {
     let helloMessage = 'Hello World!!';
     res.send(helloMessage)
+})
+app.get('/videos', (req: Request, res: Response) => {
+    res.send(videos)
+})
+app.get('/videos/:videoId', (req: Request, res: Response) => {
+    const id = +req.params.videoId;
+    const video = videos.find(v => v.id === id)
+    if (!video) {
+        res.sendStatus(404)
+    } else {
+        // res.send(video)
+        res.json(video)
+    }
+})
+app.post('/videos', (req:Request, res:Response) => {
+    const newVideo = {
+        id: (+new Date()),
+        title: req.body.title,
+        author: 'I\'m',
+    }
+    videos.push(newVideo)
+    res.send(newVideo)
+})
+app.delete('/videos/:videoId', (req:Request, res:Response) => {
+    const id = +req.params.videoId;
+    const index = videos.findIndex(v => v.id === id)
+    if (index === -1) {
+        res.sendStatus(404)
+    } else {
+        videos.splice(index, 1)
+        res.sendStatus(204)
+    }
+})
+app.put('/videos/:videoId', (req:Request, res:Response) => {
+    const id = +req.params.videoId;
+    const video = videos.find(v => v.id === id)
+    if (!video) {
+        res.sendStatus(404)
+    } else {
+        video.title = req.body.title
+        res.sendStatus(204)
+    }
 })
 
 app.listen(port, () => {
@@ -12,6 +67,5 @@ app.listen(port, () => {
 })
 
 
-
 //              yarn nodemon index.js
-//              yarn nodemon --inspect index.js
+//              yarn nodemon --inspect dist/index.js
