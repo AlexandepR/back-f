dcb0bceb2d9a579a67c97e5a74851d8d5fa3912e
Alex <alexandepp@gmail.com>
2022-06-13 07:54:54 +0300

    middlewareCounter

--- first-back-end/dist/index.js
+++ first-back-end/dist/index.js
@@ -12,8 +12,39 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 // app.use(bodyParser.json())
 const port = process.env.PORT || 3001;
+let blablaMiddleware = (req, res, next) => {
+    // @ts-ignore
+    req.blabla = "hello";
+    next();
+};
+let authGuardMiddleware = (req, res, next) => {
+    if (req.query.token === "123") {
+        next();
+    }
+    else {
+        res.send(401);
+    }
+};
+let requestCounter = 0;
+const requestCounterMiddleware = (req, res, next) => {
+    requestCounter++;
+    next();
+};
+app.use(requestCounterMiddleware);
+app.use(blablaMiddleware);
+app.use(authGuardMiddleware);
 const parserMiddleware = (0, body_parser_1.default)({});
 app.use(parserMiddleware);
+app.get('/products', blablaMiddleware, (req, res) => {
+    // @ts-ignore
+    const blabla = req.blabla;
+    res.send({ value: blabla + '!!!!' });
+});
+app.get('/users', (req, res) => {
+    // @ts-ignore
+    const blabla = req.blabla;
+    res.send({ value: blabla + ' from users' });
+});
 app.use('/products', products_router_1.productsRouter);
 app.use('/addresses', addresses_router_1.addressesRouter);
 app.listen(port, () => {
--- first-back-end/src/index.ts
+++ first-back-end/src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express'
+import express, {NextFunction, Request, Response} from 'express'
 import cors from 'cors'
 import bodyParser from "body-parser";
 import {productsRouter} from "./routes/products-router";
@@ -9,9 +9,45 @@ app.use(cors())
 // app.use(bodyParser.json())
 const port = process.env.PORT || 3001
 
+let blablaMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    // @ts-ignore
+    req.blabla = "hello"
+    next()
+};
+let authGuardMiddleware = (req: Request,res: Response, next: NextFunction) => {
+    if (req.query.token === "123") {
+        next()
+    } else {
+        res.send(401)
+    }
+}
+
+let requestCounter = 0
+
+const requestCounterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    requestCounter++;
+    next()
+}
+
+app.use(requestCounterMiddleware)
+app.use(blablaMiddleware)
+app.use(authGuardMiddleware)
+
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
 
+
+app.get('/products', blablaMiddleware,(req: Request, res: Response) => {
+    // @ts-ignore
+    const blabla = req.blabla;
+    res.send({value: blabla + '!!!!'})
+})
+
+app.get('/users', (req: Request, res: Response) => {
+    // @ts-ignore
+    const blabla = req.blabla;
+    res.send({value: blabla + ' from users'})
+})
 
 app.use('/products', productsRouter)
 app.use('/addresses', addressesRouter)
